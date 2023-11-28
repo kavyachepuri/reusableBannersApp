74671396a5404904502a042e96edbc9c2701b3e0
kavya-chepuri <kavyachepuri777@gmail.com>
2023-11-28 13:08:54 +0000

    reusableBannersApp

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import BannerCardItem from './components/BannerCardItem'
+import './App.css'
+
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
+
+const App = () => (
+  <div>
+    <ul>
+      {bannerCardsList.map(eachBanner => (
+        <BannerCardItem bannerDetails={eachBanner} key={eachBanner.id} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here.
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerDetails} = props
+  const {headerText, description, className} = bannerDetails
+
+  return (
+    <li className={`${className}`}>
+      <div>
+        <h1>{headerText}</h1>
+        <p>{description}</p>
+        <button type="button">Show More</button>
+      </div>
+    </li>
+  )
+}
+export default BannerCardItem
